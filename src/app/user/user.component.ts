4195b5b7d933572c4b91fa76df424307fcb2bceb
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-09-18 18:23:25 -0500

    project working

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
 })
 export class UserComponent implements OnInit {
+  allowNewUser = false;
+  userCreationStatus = "No User was created";
+  userName = "Jeff Winkler";
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewUser = true;
+    }, 2000);
   }
 
+  ngOnInit() {}
+
+  onCreateUser() {
+    this.userCreationStatus = "User was created!";
+  }
+
+  onUpdateUser() {}
 }
