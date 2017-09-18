16d8250ea46c5edce98f07be2fffcc7279503ba7
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-09-18 17:09:23 -0500

    bootstrap added and working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// ======= Modules ===========
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AlertModule } from "ngx-bootstrap";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { TooltipModule } from "ngx-bootstrap/tooltip";
+import { ModalModule } from "ngx-bootstrap/modal";
 
-import { AppComponent } from './app.component';
+// ========== Components ===========
+import { AppComponent } from "./app.component";
+import { UserComponent } from "./user/user.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, UserComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AlertModule,
+    BsDropdownModule,
+    TooltipModule,
+    ModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
